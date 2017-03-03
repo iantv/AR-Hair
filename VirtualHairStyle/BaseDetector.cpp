ead10e9f3f7301924d81324ac81efb145827cf1a
iantv <ian.tatiana.v@gmail.com>
2017-03-03 17:28:41 +1000

    Add detector classes, which use detectMultiScale and tracking

---
+++ VirtualHairStyle/BaseDetector.cpp
@@ -0,0 +1,241 @@
+#include "BaseDetector.h"
+#include <vector>
+
+using namespace std;
+
+BaseDetector::BaseDetector()
+{
+	_isDetected == false;
+	_frame_count = 0;
+}
+
+BaseDetector::~BaseDetector()
+{
+
+}
+
+int BaseDetector::load_cascade()
+{
+	if (!_cascade.load(_cascadeName)) {
+		printf("--(!)Error loading %s", _cascadeName);
+		return 0;
+	};
+	return 1;
+}
+
+FaceDetector::FaceDetector() : BaseDetector()
+{
+	_cascadeName = "haarcascades/haarcascade_frontalface_alt.xml";
+	_tracker = Tracker::create("MEDIANFLOW");
+}
+
+FaceDetector::~FaceDetector()
+{
+
+}
+
+bool FaceDetector::try_detect(Mat im, Mat frame_gray, Rect2d *rect, Size minSize, Size maxSize)
+{
+	if (_frame_count == FACE_DETECTION_INTERVAL) {
+		_frame_count = 0;
+		_isDetected = false;
+	}
+	if (!_isDetected) {
+		if (!detect_and_tracker(im, frame_gray)) return false;
+	} else {
+		_isDetected = _tracker->update(im, _detectedRect);
+		if (!_isDetected && (!detect_and_tracker(im, frame_gray))) return false;
+	}
+	*rect = _detectedRect;
+	_frame_count++;
+	return true;
+}
+
+bool FaceDetector::detect_and_tracker(Mat im, Mat frame_gray, Size minSize, Size maxSize)
+{
+	vector<Rect> faces;
+	_cascade.detectMultiScale(
+		frame_gray, faces, 1.3, 5, 0 | CASCADE_SCALE_IMAGE, minSize, maxSize);
+	if (faces.size() == 0) return false;
+	_detectedRect = faces[0];
+	_tracker = Tracker::create("MEDIANFLOW");
+	_isDetected = _tracker->init(im, _detectedRect);
+	return true;
+}
+
+FaceElementsDetector::FaceElementsDetector() : BaseDetector()
+{
+
+}
+
+FaceElementsDetector::~FaceElementsDetector()
+{
+
+}
+
+NoseDetector::NoseDetector() : FaceElementsDetector()
+{
+	_cascadeName = "haarcascades/haarcascade_mcs_nose.xml";
+	_tracker = Tracker::create("KCF");
+}
+
+NoseDetector::~NoseDetector()
+{
+
+}
+
+bool NoseDetector::try_detect(Mat im, Mat frame_gray, Rect2d *rect, Size minSize, Size maxSize)
+{
+	if (_frame_count == NOSE_DETECTION_INTERVAL) {
+		_frame_count = 0;
+		_isDetected = false;
+	}
+	vector<Rect> nose;
+	if (!_isDetected) {
+		_cascade.detectMultiScale(frame_gray, nose, 1.1, 3, 0 | CASCADE_SCALE_IMAGE, minSize, maxSize);
+		//call detect and tracker
+		if (!nose.size()) return false;
+		_isDetected = true;
+		_detectedRect = nose[0];
+		_tracker = Tracker::create("KCF");
+		_isDetected = _tracker->init(im, _detectedRect);
+	} else {
+		_isDetected = _tracker->update(im, _detectedRect);
+	}
+	*rect = _detectedRect;
+	_frame_count++;
+	return true;
+}
+
+bool NoseDetector::detect_and_tracker(Mat im, Mat frame_gray, Size minSize, Size maxSize)
+{
+	vector<Rect> nose;
+	_cascade.detectMultiScale(frame_gray, nose, 1.1, 3, 0 | CASCADE_SCALE_IMAGE, minSize, maxSize);
+	if (!nose.size()) return false;
+	_isDetected = true;
+	_detectedRect = nose[0];
+	_tracker = Tracker::create("KCF");
+	_isDetected = _tracker->init(im, _detectedRect);
+	return true;
+}
+
+MouthDetector::MouthDetector() : FaceElementsDetector()
+{
+	_cascadeName = "haarcascades/haarcascade_mcs_mouth.xml";
+	_tracker = Tracker::create("KCF");
+}
+
+MouthDetector::~MouthDetector()
+{
+
+}
+
+bool MouthDetector::try_detect(Mat im, Mat frame_gray, Rect2d *rect, Size minSize, Size maxSize)
+{
+	if (_frame_count == MOUTH_DETECTION_INTERVAL) {
+		_frame_count = 0;
+		_isDetected = false;
+	}
+	vector<Rect> mouth;
+	if (!_isDetected) {
+		_cascade.detectMultiScale(frame_gray, mouth, 1.1, 3, 0 | CASCADE_SCALE_IMAGE, minSize, maxSize);
+		//call detect and tracker
+		if (!mouth.size()) return false;
+		_isDetected = true;
+		_detectedRect = mouth[0];
+		_tracker = Tracker::create("KCF");
+		_isDetected = _tracker->init(im, _detectedRect);
+	}
+	else {
+		_isDetected = _tracker->update(im, _detectedRect);
+	}
+	*rect = _detectedRect;
+	_frame_count++;
+	return true;
+}
+
+bool MouthDetector::detect_and_tracker(Mat im, Mat frame_gray, Size minSize, Size maxSize)
+{
+	vector<Rect> mouth;
+	_cascade.detectMultiScale(frame_gray, mouth, 1.1, 3, 0 | CASCADE_SCALE_IMAGE, minSize, maxSize);
+	if (!mouth.size()) return false;
+	_isDetected = true;
+	_detectedRect = mouth[0];
+	_tracker = Tracker::create("KCF");
+	_isDetected = _tracker->init(im, _detectedRect);
+	return true;
+}
+
+PairFaceElementsDetector::PairFaceElementsDetector() : FaceElementsDetector()
+{
+
+}
+
+PairFaceElementsDetector::~PairFaceElementsDetector()
+{
+
+}
+
+EyesDetector::EyesDetector() : PairFaceElementsDetector()
+{
+	_cascadeName = "haarcascades/haarcascade_eye.xml";
+	_leftTracker = Tracker::create("KCF");
+	_rightTracker = Tracker::create("KCF");
+}
+
+EyesDetector::~EyesDetector()
+{
+
+}
+
+bool EyesDetector::try_detect_pair(Mat im, Mat frame_gray, Rect2d faceRect, Size minSize, Size maxSize)
+{
+	if (_frame_count == EYES_DETECTION_INTERVAL) {
+		_frame_count = 0;
+		_isDetected = _isDetectedLeft = _isDetectedRight = false;
+	}
+	
+	if (!_isDetected) {
+		vector<Rect> eyes;
+		_cascade.detectMultiScale(frame_gray, eyes, 1.1, 3, 0 | CASCADE_SCALE_IMAGE, minSize, maxSize);
+		for (size_t j = 0; j < eyes.size(); j++) {
+			add_eye_point(im, faceRect, eyes[j]);
+			if (_isDetectedLeft && _isDetectedRight) break;
+		}
+		if (eyes.size() < 2) return false;
+		_isDetected = _isDetectedLeft && _isDetectedRight;
+	} else {
+		if (_isDetectedLeft = _leftTracker->update(im, _detectedLeftRect))
+			add_eye_point(im, faceRect, _detectedLeftRect);
+		if (_isDetectedRight = _rightTracker->update(im, _detectedRightRect))
+			add_eye_point(im, faceRect, _detectedRightRect);
+	}
+	_frame_count++;
+	return true;
+}
+
+bool EyesDetector::detect_and_tracker(Mat im, Mat frame_gray, Size minSize, Size maxSize)
+{
+	return true;
+}
+
+void EyesDetector::add_eye_point(cv::Mat im, Rect face, Rect eye)
+{
+	if (eye.y + eye.height*0.5 > face.height * 0.5) return;
+	if (is_left_eye(eye, face) && !_isDetectedLeft) {
+		_isDetectedLeft = true;
+		_detectedLeftRect = eye;
+		_leftTracker = Tracker::create("KCF");
+		_leftTracker->init(im, _detectedLeftRect);
+	} else if (!is_left_eye(eye, face) && !_isDetectedRight) {
+		_isDetectedRight = true;
+		_detectedRightRect = eye;
+		_rightTracker = Tracker::create("KCF");
+		_rightTracker->init(im, _detectedRightRect);
+	}
+}
+
+bool EyesDetector::is_left_eye(Rect eye, Rect face)
+{
+	return eye.x + eye.width*0.5 < face.width*0.5;
+}
---
+++ VirtualHairStyle/BaseDetector.h
@@ -0,0 +1,104 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <opencv2/opencv.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/tracking.hpp>
+#include <opencv/cxcore.h>
+
+using namespace cv;
+
+#define FACE_DETECTION_INTERVAL 100
+#define NOSE_DETECTION_INTERVAL 1
+#define MOUTH_DETECTION_INTERVAL 100
+#define EYES_DETECTION_INTERVAL 1
+
+class BaseDetector
+{
+public:
+	BaseDetector();
+	~BaseDetector();
+	int load_cascade();
+	virtual Rect2d getDetectedRect() const { return _detectedRect; }
+protected:
+	CascadeClassifier _cascade;
+	String _cascadeName;
+	Ptr<Tracker> _tracker;
+	Rect2d _detectedRect;
+	bool _isDetected;
+	int _frame_count;
+};
+
+class FaceDetector : public BaseDetector
+{
+public:
+	FaceDetector();	
+	~FaceDetector();
+	bool try_detect(Mat im, Mat frame_gray, Rect2d *rect, Size minSize = Size(), Size maxSize = Size());
+private:
+	bool detect_and_tracker(Mat im, Mat frame_gray, Size minSize = Size(), Size maxSize = Size());
+};
+
+class FaceElementsDetector : public BaseDetector
+{
+public:
+	FaceElementsDetector();
+	~FaceElementsDetector();
+	virtual bool try_detect(Mat im, Mat frame_gray, Rect2d *rect, Size minSize = Size(), Size maxSize = Size()) { return false; };
+private:
+
+};
+
+class NoseDetector : public FaceElementsDetector
+{
+public:
+	NoseDetector();
+	~NoseDetector();
+	bool try_detect(Mat im, Mat frame_gray, Rect2d *rect, Size minSize = Size(), Size maxSize = Size()) override;
+private:
+	bool detect_and_tracker(Mat im, Mat frame_gray, Size minSize = Size(), Size maxSize = Size());
+};
+
+class MouthDetector : public FaceElementsDetector
+{
+public:
+	MouthDetector();
+	~MouthDetector();
+	bool try_detect(Mat im, Mat frame_gray, Rect2d *rect, Size minSize = Size(), Size maxSize = Size()) override;
+private:
+	bool detect_and_tracker(Mat im, Mat frame_gray, Size minSize = Size(), Size maxSize = Size());
+};
+
+class PairFaceElementsDetector : public FaceElementsDetector
+{
+public:
+	PairFaceElementsDetector();
+	~PairFaceElementsDetector();
+	virtual bool try_detect_pair(Mat im, Mat frame_gray, Rect2d faceRect, Size minSize = Size(), Size maxSize = Size()) { return false; }
+	Rect2d getLeftDetectedRect() { return _detectedLeftRect; }
+	Rect2d getRightDetectedRect() { return _detectedRightRect; }
+protected:
+	Ptr<Tracker> _leftTracker;
+	Ptr<Tracker> _rightTracker;
+	Rect2d _detectedLeftRect;
+	Rect2d _detectedRightRect;
+};
+
+class EyesDetector : public PairFaceElementsDetector
+{
+public:
+	EyesDetector();
+	~EyesDetector();
+	bool try_detect_pair(Mat im, Mat frame_gray, Rect2d faceRect, Size minSize = Size(), Size maxSize = Size()) override;
+private:
+	bool is_left_eye(Rect eye, Rect face);
+	bool detect_and_tracker(Mat im, Mat frame_gray, Size minSize = Size(), Size maxSize = Size());
+	void add_eye_point(cv::Mat im, Rect face, Rect eye);
+	bool _isDetectedLeft = false, _isDetectedRight = false;
+};
+
+extern FaceDetector faceDetector;
+extern NoseDetector noseDetector;
+extern EyesDetector eyesDetector;
+extern MouthDetector mouthDetector;
