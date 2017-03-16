343b1f75f4cd2a57e779be4384e7906f08e22ff2
iantv <ian.tatiana.v@gmail.com>
2017-03-16 18:46:54 +1000

    PairFaceElementsDetector: update try_detect_pair()
    Create tracker's for eyes in the constructor
    and after detection of eyes
    Detect eyes on the first frame or when tracking is failed
    
    Signed-off-by: iantv <ian.tatiana.v@gmail.com>

--- VirtualHairStyle/BaseDetector.cpp
+++ VirtualHairStyle/BaseDetector.cpp
@@ -162,13 +162,10 @@ PairFaceElementsDetector::~PairFaceElementsDetector()
 
 bool PairFaceElementsDetector::try_detect_pair(Mat im, Mat frame_gray, Rect2d faceRect, Size minSize, Size maxSize)
 {
-	if (!_isDetected) 
-		return detect_and_tracker(im, frame_gray, faceRect);
-	bool _isTrackedLeft = _leftTracker->update(im, _detectedLeftRect);
-	bool _isTrackedRight = _rightTracker->update(im, _detectedRightRect);
-	if (!_isTrackedLeft || !_isTrackedRight) return false;
-	add_point(im, faceRect, _detectedLeftRect);
-	add_point(im, faceRect, _detectedRightRect);
+	if (_isDetected &&
+		(_isDetected = _leftTracker->update(im, _detectedLeftRect) && _rightTracker->update(im, _detectedRightRect)))
+		return _isDetected;
+	return _isDetected = detect_and_tracker(im, frame_gray, faceRect);
 }
 
 bool PairFaceElementsDetector::is_left(Rect eye, Rect face)
@@ -193,20 +190,27 @@ void PairFaceElementsDetector::add_point(cv::Mat im, Rect face, Rect elem)
 	if (is_left(elem, face) && !_isDetectedLeft) {
 		_isDetectedLeft = true;
 		_detectedLeftRect = Rect2d(elem.x + face.x, elem.y + face.y, elem.width, elem.height);
+		_leftTracker = Tracker::create(_trackerName);
 		_leftTracker->init(im, _detectedLeftRect);
 	} else if (!is_left(elem, face) && !_isDetectedRight) {
 		_isDetectedRight = true;
 		_detectedRightRect = Rect2d(elem.x + face.x, elem.y + face.y, elem.width, elem.height);
+		_rightTracker = Tracker::create(_trackerName);
 		_rightTracker->init(im, _detectedRightRect);
 	}
 	_isDetected = _isDetectedLeft && _isDetectedRight;
 }
 
+void PairFaceElementsDetector::resetDetectedFlag(bool value) {
+	_isDetected = _isDetectedLeft = _isDetectedRight = value;
+}
+
 EyesDetector::EyesDetector() : PairFaceElementsDetector()
 {
 	_cascadeName = "haarcascades/haarcascade_eye.xml";
-	_leftTracker = Tracker::create("KCF");
-	_rightTracker = Tracker::create("KCF");
+	_trackerName = "KCF";
+	_leftTracker = Tracker::create(_trackerName);
+	_rightTracker = Tracker::create(_trackerName);
 	_interval = EYES_DETECTION_INTERVAL;
 }
 
